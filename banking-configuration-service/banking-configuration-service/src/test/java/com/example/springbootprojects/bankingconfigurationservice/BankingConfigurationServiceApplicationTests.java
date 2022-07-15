c97913bb987185094d16d4b7ebe7ad895ba97cf2
rnelliyantavida <ruksanaashfak8@gmail.com>
2022-07-15 16:35:45 -0400

    added cofiguration service

---
+++ banking-configuration-service/banking-configuration-service/src/test/java/com/example/springbootprojects/bankingconfigurationservice/BankingConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootprojects.bankingconfigurationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingConfigurationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
